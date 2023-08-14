a72e1ce9a62df4f37f073f196e73cc6637155220
Nami <havenothorn@gmail.com>
2023-08-14 16:52:54 +0900

    add: JavaScript 내장 객체 예제 추가 - 테스트 코드 적용

---
+++ src/__tests__/built-inQuiz0.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest";
+import { text, printedCharacters, ids } from "../built-inQuiz0";
+
+test("문자열의 모든 캐릭터를 하나씩 출력하라", () => {
+  expect(printedCharacters.join("")).toBe(text);
+});
+
+test("각각의 id를 배열로 보관하기", () => {
+  expect(ids.split(", ")).toStrictEqual(["user1", "user2", "user3", "user4"]);
+});
