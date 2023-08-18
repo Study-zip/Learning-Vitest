3095db12bac3e668fbe9768d7663008e2120d0a3
Nami <havenothorn@gmail.com>
2023-08-18 18:55:13 +0900

    test: Class 테스트 코드 수정

--- src/__tests__/class/classQuiz2.test.js
+++ src/__tests__/class/classQuiz2.test.js
@@ -1,11 +1,9 @@
 import { test, it, expect } from "vitest";
 import { worker, worker1 } from "../../class/classQuiz2";
 
-test("문제 [2] 한 달 월급을 계산하는 Class 테스트", () => {
-  it("정직원 월급 계산하기", () => {
-    expect(worker.Salary).toBe("6,000,000");
-  });
-  it("파트타임 직원 월급 계산하기", () => {
-    expect(worker1.Salary).toBe("962,000");
-  });
+test("정직원 월급 계산", () => {
+  expect(worker.Salary().toLocaleString()).toBe("6,000,000");
+});
+test("파트타임 월급 계산", () => {
+  expect(worker1.Salary().toLocaleString()).toBe("962,000");
 });
