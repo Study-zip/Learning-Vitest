56a13dc4c3303de8964efee0ad04e69e47654be0
Nami <havenothorn@gmail.com>
2023-08-19 01:29:50 +0900

    test: Test 구조 변경

--- src/__tests__/array/replaceArray.test.js
+++ src/__tests__/array/replaceArray.test.js
@@ -1,13 +1,14 @@
-import { test, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 import { replace } from "../../array/replaceArray";
 
 const array = ["🍌", "🍓", "🍇", "🍓"];
 const replaced = replace(array, "🍓", "🥝");
 
-test("주어진 배열 확인", () => {
-  expect(array).toStrictEqual(["🍌", "🍓", "🍇", "🍓"]);
-});
-
-test("새 배열 만들기", () => {
-  expect(replaced).toStrictEqual(["🍌", "🥝", "🍇", "🥝"]);
+describe("주어진 배열 수정 없이 내부 아이템 교체해서 새 배열 만들기", () => {
+  test("주어진 배열 확인", () => {
+    expect(array).toStrictEqual(["🍌", "🍓", "🍇", "🍓"]);
+  });
+  test("새 배열 확인", () => {
+    expect(replaced).toStrictEqual(["🍌", "🥝", "🍇", "🥝"]);
+  });
 });
